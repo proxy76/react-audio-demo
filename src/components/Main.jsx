8d34b8ab69cc4ff4450c535c16216bb0a05b9b7a
Radulescu Razvan <proxy.razvan@gmail.com>
2024-01-10 23:39:57 +0200

    add: all files

---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+import audioFile from "../assets/demo.mp3"
+
+export default function Main() {
+    return (
+        <>
+            <div className="wrapper">
+                <div className="text">
+                        DEMONSTRAȚIE<br></br>
+                        <span className="span-1">De fișier<br></br></span>
+                        AUDIO
+                </div>
+                <div className="main">
+                    <audio controls>
+                        <source src={audioFile} type="audio/mp3" />
+                    </audio>
+                </div>
+            </div>
+            
+        </>
+    )
+}
